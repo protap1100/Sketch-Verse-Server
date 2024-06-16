bf0d73d003e8bc9fd786fed4b5ada8232e45b242
protap1100 <protapb23@gmail.com>
2024-06-16 18:31:16 +0600

    some fixes

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
 // const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 5001;
 
-// Middleware 
+// Middleware
 app.use(cors());
+// app.use(cors({
+//   origin:['https://server-site-navy.vercel.app'],
+//   credentials: true,
+// }))
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hnusrai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -18,121 +22,119 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
+  const paintCollection = client.db("paintDB").collection("paint");
+  const paintCategoryCollection = client.db("paintDB").collection("myPaint");
 
-    const paintCollection = client.db('paintDB').collection('paint');
-    const paintCategoryCollection = client.db('paintDB').collection('myPaint');
-
-    // Getting Data 
-    app.get('/allPaint', async(req,res)=>{
-      const cursor = paintCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    })
-
-    app.get('/allHomePaint', async(req,res)=>{
-      const cursor = paintCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-
-    // Category data 
-    app.get('/categoryPaint', async(req,res)=>{
-      const cursor = paintCategoryCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    })
-
-    app.get('/paintDetails/:id', async (req, res) => {
-      const paintId = req.params.id;
-      // const result = await paintCollection.findOne({ _id: new ObjectId(paintId) });
-      const query = {_id: new ObjectId(paintId) }
-      const result = await paintCollection.findOne(query)
-      res.send(result);
-    });
-
-    // Getting Single Data for Updating Id 
-    app.get('/updatingPaint/:id', async (req, res) => {
-      const paintId = req.params.id;
-      // const result = await paintCollection.findOne({ _id: new ObjectId(paintId) });
-      const query = {_id: new ObjectId(paintId) }
-      const result = await paintCollection.findOne(query)
-      res.send(result);
-    });
-
-    // Posting email 
-    app.post('/myPaint/:email', async (req, res) => {
-      const userEmail = req.params.email;
-      const newPaint = req.body;
-      newPaint.email = userEmail; // Add the user's email to the paint object
-      const result = await paintCollection.insertOne(newPaint);
-      res.send(result);
-    });
-
-    // Getting Data According to email
-    app.get('/myPaint/:email', async (req, res) => {
-      const userEmail = req.params.email;
-      const query = { email: userEmail };
-      const cursor = paintCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
+  // Getting Data
+  app.get("/allPaint", async (req, res) => {
+    const cursor = paintCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
   });
-  
 
-    // Updating
-    app.put('/updatingPaint/:id', async(req, res) => {
-      const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
-      console.log(filter); 
-      const options = { upsert: true };
-      const updatePaint = req.body;
-      console.log(updatePaint)
-      const paint = {
-          $set: {
-            name: updatePaint.name,
-            category : updatePaint.category,
-            customization : updatePaint.customization,
-            description: updatePaint.description,
-            image: updatePaint.image,
-            price: updatePaint.price,
-            processTime : updatePaint.processTime,
-            rating : updatePaint.rating,
-            stockStatus: updatePaint.stockStatus
-          }
-      }
-      // console.log(paint)
-      const result = await paintCollection.updateOne(filter, paint, options);
-      res.send(result);
-      })
+  app.get("/allHomePaint", async (req, res) => {
+    const cursor = paintCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+  });
 
+  // Category data
+  app.get("/categoryPaint", async (req, res) => {
+    const cursor = paintCategoryCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+  });
 
-    app.post('/allPaint', async(req, res)=>{
-        const newPaint =  req.body;
-        console.log(newPaint);
-        const result  = await paintCollection.insertOne(newPaint);
-        res.send(result)
-    }) 
-    
+  app.get("/paintDetails/:id", async (req, res) => {
+    const paintId = req.params.id;
+    // const result = await paintCollection.findOne({ _id: new ObjectId(paintId) });
+    const query = { _id: new ObjectId(paintId) };
+    const result = await paintCollection.findOne(query);
+    res.send(result);
+  });
 
-    // Deleting Single Data 
-    app.delete('/allPaint/:id', async(req,res)=>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await paintCollection.deleteOne(query);
-        res.send(result);
-    })
+  // Posting email
+  app.post("/myPaint/:email", async (req, res) => {
+    const userEmail = req.params.email;
+    const newPaint = req.body;
+    console.log("post", userEmail);
+    newPaint.email = userEmail; // Add the user's email to the paint object
+    const result = await paintCollection.insertOne(newPaint);
+    res.send(result);
+  });
 
-    
+  // Getting Data According to email
+  app.get("/myPaint/:email", async (req, res) => {
+    const userEmail = req.params.email;
+    const query = { email: userEmail };
+    console.log("get", userEmail);
+    const cursor = paintCollection.find(query);
+    const result = await cursor.toArray();
+    res.send(result);
+  });
+
+  // Getting Single Data for Updating Id
+  app.get("/updatingPaint/:id", async (req, res) => {
+    const paintId = req.params.id;
+    // const result = await paintCollection.findOne({ _id: new ObjectId(paintId) });
+    const query = { _id: new ObjectId(paintId) };
+    const result = await paintCollection.findOne(query);
+    res.send(result);
+  });
+
+  // Updating
+  app.put("/updatingPaint/:id", async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    console.log(filter);
+    const options = { upsert: true };
+    const updatePaint = req.body;
+    console.log(updatePaint);
+    const paint = {
+      $set: {
+        name: updatePaint.name,
+        category: updatePaint.category,
+        customization: updatePaint.customization,
+        description: updatePaint.description,
+        image: updatePaint.image,
+        price: updatePaint.price,
+        processTime: updatePaint.processTime,
+        rating: updatePaint.rating,
+        stockStatus: updatePaint.stockStatus,
+      },
+    };
+    // console.log(paint)
+    const result = await paintCollection.updateOne(filter, paint, options);
+    res.send(result);
+  });
+
+  app.post("/allPaint", async (req, res) => {
+    const newPaint = req.body;
+    console.log(newPaint);
+    const result = await paintCollection.insertOne(newPaint);
+    res.send(result);
+  });
+
+  // Deleting Single Data
+  app.delete("/allPaint/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await paintCollection.deleteOne(query);
+    res.send(result);
+  });
 
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -140,16 +142,14 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-// Middleware 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res)=>{
-    res.send('Painting Website is running');
-})
+app.get("/", (req, res) => {
+  res.send("Painting Website is running");
+});
 
-app.listen(port,()=>{
-   console.log(`this is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`this is running on port ${port}`);
+});
